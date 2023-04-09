d609720d583efd6b699d49f109e5778b7bc14bc9
kebin20 <ktanzyl@hotmail.com>
2023-04-09 17:34:19 +0900

    chore: added navbar to app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "@/styles/Home.module.css";
+import NavBar from "@/components/layout/Navbar";
 
 export default function Home() {
   return (
@@ -10,6 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <NavBar />
       <main>
         <h1>PayAPI Website</h1>
       </main>
