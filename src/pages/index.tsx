7cac1fba64e6f09f5c6b997c9b4cb203f136a359
kebin20 <ktanzyl@hotmail.com>
2023-05-21 13:04:10 +0900

    style: styled hero container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import NavBar from "@/components/layout/Navbar";
+import Image from "next/image";
+
+import PhoneMockup from "../assets/home/desktop/illustration-phone-mockup.svg";
 
 export default function Home() {
   return (
@@ -11,7 +14,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavBar />
-      <main className=""></main>
+      <main>
+        <div className="relative">
+          <Image
+            alt="build your apis for free"
+            src={PhoneMockup}
+            className="left-1/2 -translate-x-1/2 -translate-y-[5%] w-1/2"
+          />
+
+          <h1>Start building with our APIs for absolutely free.</h1>
+
+
+        </div>
+      </main>
     </>
   );
 }
