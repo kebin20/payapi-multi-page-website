dd17d5c1b11c2c1b6625e65020f91cf99379baa0
kebin20 <ktanzyl@hotmail.com>
2023-06-04 21:34:24 +0900

    chore: compartmentalised CTA

---
+++ src/components/layout/CallToAction.tsx
@@ -0,0 +1,17 @@
+import { PrimaryButton } from "../UI/Buttons";
+
+export default function CallToAction() {
+  return (
+    <section className="mx-6 mb-20 grid gap-8 text-center">
+      <h2 className="text-3xl  text-blue font-bold leading-9 mx-4">
+        Ready to start?
+      </h2>
+      <input
+        type="text"
+        placeholder="Enter email adddress"
+        className="bg-white font-bold px-14 py-3.5 rounded-3xl shadow-lg"
+      />
+      <PrimaryButton>Schedule a Demo</PrimaryButton>
+    </section>
+  );
+}
