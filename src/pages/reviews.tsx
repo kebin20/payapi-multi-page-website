d57755ec38352b1cb7219f164911f0ae72a12785
kebin20 <ktanzyl@hotmail.com>
2023-10-18 15:06:19 +0900

    chore: created dummy data for reviews

---
+++ src/pages/reviews.tsx
@@ -0,0 +1,31 @@
+import ReviewList from "../components/reviews/ReviewList";
+
+const DUMMY_REVIEWS = [
+  {
+    id: "r1",
+    title: "Great API for your business needs",
+    comment:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptatum sunt optio nemo minus, ad ipsa obcaecati, tempora eaque voluptas sequi reprehenderit consectetur, nostrum amet praesentium et soluta sit quibusdam?",
+    date: "September 1st 2023",
+  },
+  {
+    id: "r2",
+    title: "It's just an ok API",
+    comment:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptatum sunt optio nemo minus, ad ipsa obcaecati, tempora eaque voluptas sequi reprehenderit consectetur, nostrum amet praesentium et soluta sit quibusdam?",
+    date: "September 1st 2023",
+  },
+  {
+    id: "r3",
+    title: "DONT USE WARNING, CRASHED OUR SERVERS",
+    comment:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptatum sunt optio nemo minus, ad ipsa obcaecati, tempora eaque voluptas sequi reprehenderit consectetur, nostrum amet praesentium et soluta sit quibusdam?",
+    date: "September 1st 2023",
+  },
+];
+
+function Reviews() {
+  return <ReviewList reviews={DUMMY_REVIEWS} />;
+}
+
+export default Reviews;
