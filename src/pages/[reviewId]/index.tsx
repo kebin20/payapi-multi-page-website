212a6ff21b597b7fb1cd4a8121e81f5b9ef39d6d
kebin20 <ktanzyl@hotmail.com>
2023-10-20 13:13:17 +0900

    chore: added dummy data to review item

---
+++ src/pages/[reviewId]/index.tsx
@@ -0,0 +1,11 @@
+function FullReview() {
+  return (
+    <>
+      <span>⭐⭐⭐⭐</span>
+      <h1>Some Review Here</h1>
+      <p>By Someone</p>
+      <span>1st January 2000</span>
+      <p>Full comment here</p>
+    </>
+  );
+}
