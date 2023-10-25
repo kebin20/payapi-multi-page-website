ba66a9c0278c94d9c5ecc689b063f9f8c9db388b
kebin20 <ktanzyl@hotmail.com>
2023-10-25 16:49:41 +0900

    chore: changed fetching data from dummy data to database in MongoDB

--- src/pages/reviews.tsx
+++ src/pages/reviews.tsx
@@ -1,3 +1,5 @@
+import { MongoClient } from "mongodb";
+
 import ReviewList from "../components/reviews/ReviewList";
 
 const DUMMY_REVIEWS = [
@@ -40,9 +42,26 @@ export default Reviews;
 
 export async function getStaticProps() {
   // fetch data from an API
+  const client = await MongoClient.connect(
+    "mongodb+srv://generaluser:1234@cluster0.ulhesxn.mongodb.net/?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const reviewsCollection = db.collection("reviews");
+
+  const reviews = await reviewsCollection.find().toArray();
+
+  client.close();
+
   return {
     props: {
-      reviews: DUMMY_REVIEWS,
+      reviews: reviews.map((review) => ({
+        title: review.title,
+        name: review.name,
+        comment: review.comment,
+        date: review.date,
+        id: review._id.toString(),
+      })),
     },
   };
 }
