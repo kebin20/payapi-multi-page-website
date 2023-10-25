be6827042259f3051dc17363aa12eed3d4436333
kebin20 <ktanzyl@hotmail.com>
2023-10-25 16:51:44 +0900

    feat: enabled data fetching for individual reviews

--- src/pages/[reviewId]/index.tsx
+++ src/pages/[reviewId]/index.tsx
@@ -1,11 +1,39 @@
 import FullReviewDetail from "@/components/reviews/FullReview";
+import { MongoClient, ObjectId } from "mongodb";
 
-function FullReviewDetails() {
+function FullReviewDetails(props) {
   return (
     <>
-      <FullReviewDetail title="" author="" date="" comment="" />
+      <FullReviewDetail
+        title={props.title}
+        author={props.author}
+        date={props.date}
+        comment={props.comment}
+      />
     </>
   );
+}
+
+export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://generaluser:1234@cluster0.ulhesxn.mongodb.net/?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const reviewsCollection = db.collection("reviews");
+
+  // If wanting to find all , can pass in an empty object, which means there's no filter criteria. Second argument can be passed which defines which fields should be extracted for every document.
+  // In this case, we only want the id, so add in the params like below
+  const reviews = await reviewsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
+  return {
+    fallback: false,
+    paths: reviews.map((review) => ({
+      params: { reviewId: review._id.toString() },
+    })),
+  };
 }
 
 export async function getStaticProps(context) {
@@ -13,17 +41,27 @@ export async function getStaticProps(context) {
 
   const reviewId = context.params.reviewId;
 
-  console.log(reviewId);
+  const client = await MongoClient.connect(
+    "mongodb+srv://generaluser:1234@cluster0.ulhesxn.mongodb.net/?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const reviewsCollection = db.collection("reviews");
+
+  const selectedReview = await reviewsCollection.findOne({
+    _id: ObjectId(reviewId),
+  });
+
+  client.close();
 
   return {
     props: {
       reviewData: {
-        id: reviewId,
-        stars: "",
-        title: "",
-        author: "",
-        date: "",
-        comment: "",
+        id: selectedReview._id.toString(),
+        title: selectedReview.title,
+        author: selectedReview.author,
+        date: selectedReview.date,
+        comment: selectedReview.comment,
       },
     },
   };
