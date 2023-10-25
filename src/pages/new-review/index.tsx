a6d506b33a422523c784f1918f20e0e5c86d9771
kebin20 <ktanzyl@hotmail.com>
2023-10-25 16:49:00 +0900

    feaqt: added a POST method to push entered data into the database

--- src/pages/new-review/index.tsx
+++ src/pages/new-review/index.tsx
@@ -1,8 +1,24 @@
+import { useRouter } from "next/router";
+
 import NewReviewForm from "@/components/reviews/NewReviewForm";
 
 function NewReviewPage() {
-  function addReviewHandler(enteredReviewData) {
-    console.log(enteredReviewData);
+  const router = useRouter();
+
+  async function addReviewHandler(enteredReviewData) {
+    const response = await fetch("/api/new-review", {
+      method: "POST",
+      body: JSON.stringify(enteredReviewData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push("/");
   }
 
   return <NewReviewForm onAddReview={addReviewHandler} />;
