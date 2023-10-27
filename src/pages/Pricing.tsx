c1de51dc89995865d62b5ec2111e34eff8382731
kebin20 <ktanzyl@hotmail.com>
2023-10-27 12:04:40 +0900

    style: changed layout for desktop view

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -9,21 +9,21 @@ function Pricing() {
     <div className="pricing-main">
       <NavBar />
       <main className="text-blue">
-        <h1 className="text-3xl text-center font-bold md:text-5xl md:pt-6">
+        <h1 className="text-3xl text-center font-bold md:text-5xl md:pt-6 lg:text-left lg:mx-44 lg:text-750">
           Pricing
         </h1>
-        <div className="mb-24 md:flex md:flex-row md:justify-evenly md:gap-4 md:m-20">
+        <div className="mb-24 md:flex md:flex-row md:justify-evenly md:gap-4 md:m-20 lg:mx-44 lg:justify-normal lg:gap-20">
           {" "}
-          <div className="flex flex-col items-center gap-4">
-            <h2 className="text-2xl text-dark-pink p-14 font-bold md:p-0">
+          <div className="flex flex-col items-center gap-4 lg:items-baseline">
+            <h2 className="text-2xl text-dark-pink p-14 font-bold md:p-0 lg:text-550">
               Free Plan
             </h2>
-            <p className="hidden md:block md:text-center">
+            <p className="hidden md:block md:text-center lg:text-left">
               Build and test using our core set of products with up to 100 API
               requests
             </p>
             <h3 className="text-6xl font-bold md:text-5xl md:py-2">$0.00</h3>
-            <ul className="flex flex-col gap-4 border-y-2 border-gray p-6 px-24 md:px-12">
+            <ul className="flex flex-col gap-4 border-y-2 border-blue/25 p-6 px-24 md:px-12 lg:pl-0 lg:pr-64">
               <div className="flex  gap-4">
                 <TickIcon hidden={undefined} />
                 <li>Transactions</li>
@@ -53,20 +53,20 @@ function Pricing() {
                 <li className="opacity-50">Income</li>
               </div>
             </ul>
-            <SecondaryButton className="border-blue text-blue">
+            <SecondaryButton className="border-blue text-blue lg:mt-3">
               Request Access
             </SecondaryButton>
           </div>
-          <div className="flex flex-col items-center gap-4">
-            <h2 className="text-2xl text-dark-pink p-14 font-bold  md:p-0">
+          <div className="flex flex-col items-center gap-4 lg:items-baseline">
+            <h2 className="text-2xl text-dark-pink p-14 font-bold  md:p-0 lg:text-550">
               Basic Plan
             </h2>
-            <p className="hidden md:block md:text-center">
+            <p className="hidden md:block md:text-center lg:text-left">
               Launch your project with unlimited requests and no contractual
               minimums
             </p>
             <h3 className="text-6xl font-bold md:text-5xl md:py-2">$249.00</h3>
-            <ul className="flex flex-col gap-4 border-y-2 border-gray p-6 px-24 md:px-12">
+            <ul className="flex flex-col gap-4 border-y-2 border-blue/25 p-6 px-24 md:px-12 lg:pl-0 lg:pr-64">
               <div className="flex  gap-4">
                 <TickIcon hidden={undefined} />
                 <li>Transactions</li>
@@ -96,22 +96,22 @@ function Pricing() {
                 <li className="opacity-50">Income</li>
               </div>
             </ul>
-            <SecondaryButton className="border-blue text-blue">
+            <SecondaryButton className="border-blue text-blue lg:mt-3">
               Request Access
             </SecondaryButton>
           </div>
-          <div className="flex flex-col items-center gap-4">
-            <h2 className="text-2xl text-dark-pink p-14 font-bold md:p-0">
+          <div className="flex flex-col items-center gap-4 lg:items-baseline">
+            <h2 className="text-2xl text-dark-pink p-14 font-bold md:p-0 lg:text-550">
               Premium Plan
             </h2>
-            <p className="hidden md:block md:text-center">
+            <p className="hidden md:block md:text-center lg:text-left">
               Get tailored solutions, volume pricing, and dedicated support for
               your team
             </p>
             <h3 className="text-6xl text-blue font-bold md:text-5xl md:py-2">
               $499.00
             </h3>
-            <ul className="flex flex-col gap-4 border-y-2 border-gray p-6 px-24 md:px-12">
+            <ul className="flex flex-col gap-4 border-y-2 border-blue/25 p-6 px-24 md:px-12 lg:pl-0 lg:pr-64">
               <div className="flex  gap-4">
                 <TickIcon hidden={undefined} />
                 <li>Transactions</li>
@@ -141,7 +141,7 @@ function Pricing() {
                 <li>Income</li>
               </div>
             </ul>
-            <SecondaryButton className="border-blue text-blue">
+            <SecondaryButton className="border-blue text-blue lg:mt-3">
               Request Access
             </SecondaryButton>
           </div>
