7dd45f7e998bdfc11900030348d4a3b0db96731d
kebin20 <ktanzyl@hotmail.com>
2023-10-27 15:40:09 +0900

    chore: separated contact form into its own component

---
+++ src/components/layout/ContactForm.tsx
@@ -0,0 +1,123 @@
+import { useState } from "react";
+import { SecondaryButton } from "../UI/Buttons";
+
+function ContactForm() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    company: "",
+    title: "",
+    message: "",
+    subscribe: true,
+  });
+
+  function handleChange(event: { target: { name: any; value: any } }) {
+    setFormData((currData) => {
+      return { ...currData, [event.target.name]: event.target.value };
+    });
+  }
+
+  console.log(formData.name, formData.email);
+
+  return (
+    <>
+      <form
+        className="flex flex-col gap-4 text-blue mx-6 my-14 md:mx-44"
+        action=""
+      >
+        <div className="flex justify-between border-b-2 border-blue border-opacity-30 p-5">
+          {" "}
+          <label htmlFor="name" className="opacity-50">
+            Name
+          </label>
+          <input
+            className="bg-transparent"
+            type="text"
+            id="name"
+            name="name"
+            placeholder="John Smith"
+            onChange={handleChange}
+            value={formData.name}
+          />
+        </div>
+
+        <div className="flex justify-between  border-b-2 border-blue border-opacity-30 p-5">
+          {" "}
+          <label htmlFor="email" className="opacity-50">
+            Email Address
+          </label>
+          <input
+            className="bg-transparent"
+            type="text"
+            id="email"
+            name="email"
+            placeholder="john@email.com"
+            onChange={handleChange}
+            value={formData.email}
+          />
+        </div>
+
+        <div className="flex justify-between  border-b-2 border-blue border-opacity-30 p-5">
+          {" "}
+          <label htmlFor="company" className="opacity-50">
+            Company Name
+          </label>
+          <input
+            className="bg-transparent w-7/12"
+            type="text"
+            id="company"
+            name="company"
+            placeholder="Company limited"
+            onChange={handleChange}
+            value={formData.company}
+          />
+        </div>
+
+        <div className="flex justify-between  border-b-2 border-blue border-opacity-30 p-5">
+          {" "}
+          <label htmlFor="title" className="opacity-50">
+            Title
+          </label>
+          <input
+            className="bg-transparent"
+            type="text"
+            id="title"
+            name="title"
+            onChange={handleChange}
+            value={formData.title}
+          />
+        </div>
+
+        <div className="flex justify-between border-b-2 border-blue border-opacity-30 p-5">
+          {" "}
+          <label htmlFor="message" className="opacity-50">
+            Message
+          </label>
+          <textarea className="bg-transparent" id="message" name="message" />
+        </div>
+
+        <div className="flex gap-6 opacity-100 my-4">
+          <input
+            type="checkbox"
+            id="subscribe"
+            name="subscribe"
+            className="bg-transparent"
+            onChange={handleChange}
+          />
+          <label className="flex" htmlFor="subscribe">
+            Stay up-to-date with company announcements and updates to our API
+          </label>
+        </div>
+
+        <SecondaryButton
+          className="border-blue text-blue opacity-100 self-start"
+          type="submit"
+        >
+          Submit
+        </SecondaryButton>
+      </form>
+    </>
+  );
+}
+
+export default ContactForm;
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 import NavBar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import CallToAction from "@/components/layout/CallToAction";
-import Image from "next/image";
+import ContactForm from "@/components/layout/ContactForm";
 
 import TeslaImage from "../assets/shared/desktop/tesla-dark.svg";
 import HPImage from "../assets/shared/desktop/hewlett-dark.svg";
@@ -9,7 +11,6 @@ import GoogleImage from "../assets/shared/desktop/google-dark.svg";
 import MicrosoftImage from "../assets/shared/desktop/microsoft-dark.svg";
 import OracleImage from "../assets/shared/desktop/oracle-dark.svg";
 import NvidiaImage from "../assets/shared/desktop/nvidia-dark.svg";
-import { SecondaryButton } from "@/components/UI/Buttons";
 
 function Contact() {
   const companyLogos: any = [
@@ -28,89 +29,7 @@ function Contact() {
         <h1 className="text-4xl text-center text-blue font-bold leading-9 mx-6 md:text-5xl md:mx-44 md:mt-10">
           Submit a help request and we’ll get in touch shortly.
         </h1>
-        <form
-          className="flex flex-col gap-4 text-blue mx-6 my-14 md:mx-44"
-          action=""
-        >
-          <div className="flex justify-between border-b-2 border-blue border-opacity-30 p-5">
-            {" "}
-            <label htmlFor="name" className="opacity-50">
-              Name
-            </label>
-            <input
-              className="bg-transparent"
-              type="text"
-              id="name"
-              name="name"
-            />
-          </div>
-
-          <div className="flex justify-between  border-b-2 border-blue border-opacity-30 p-5">
-            {" "}
-            <label htmlFor="" className="opacity-50">
-              Email Address
-            </label>
-            <input
-              className="bg-transparent"
-              type="text"
-              id="email address"
-              name="email address"
-            />
-          </div>
-
-          <div className="flex justify-between  border-b-2 border-blue border-opacity-30 p-5">
-            {" "}
-            <label htmlFor="" className="opacity-50">
-              Company Name
-            </label>
-            <input
-              className="bg-transparent w-7/12"
-              type="text"
-              id="company name"
-              name="company name"
-            />
-          </div>
-
-          <div className="flex justify-between  border-b-2 border-blue border-opacity-30 p-5">
-            {" "}
-            <label htmlFor="" className="opacity-50">
-              Title
-            </label>
-            <input
-              className="bg-transparent"
-              type="text"
-              id="title"
-              name="title"
-            />
-          </div>
-
-          <div className="flex justify-between border-b-2 border-blue border-opacity-30 p-5">
-            {" "}
-            <label htmlFor="" className="opacity-50">
-              Message
-            </label>
-            <textarea className="bg-transparent" id="message" name="message" />
-          </div>
-
-          <div className="flex gap-6 opacity-100 my-4">
-            <input
-              type="checkbox"
-              id="subscribe"
-              name="subscribe"
-              className="bg-transparent"
-            />
-            <label className="flex" htmlFor="subscribe">
-              Stay up-to-date with company announcements and updates to our API
-            </label>
-          </div>
-
-          <SecondaryButton
-            className="border-blue text-blue opacity-100 self-start"
-            type="submit"
-          >
-            Submit
-          </SecondaryButton>
-        </form>
+        <ContactForm />
         <section className="grid justify-items-center gap-8 text-center px-10 py-14 md:pt-0 md:mb-10">
           <h2 className="text-blue text-2xl font-bold md:mx-40">
             Join the thousands of innovators already building with us
