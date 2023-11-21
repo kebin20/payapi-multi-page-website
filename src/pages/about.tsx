a47ed522974141504f95d19fac35676df1f1852d
kebin20 <ktanzyl@hotmail.com>
2023-11-21 15:38:49 +0900

    fix: attempt to fix casing problem with file names by re-inserting files

---
+++ src/pages/about.tsx
@@ -0,0 +1,108 @@
+/* eslint-disable react/no-unescaped-entities */
+import NavBar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
+import CallToAction from "@/components/layout/CallToAction";
+import Image from "next/image";
+import TeamMembersImage from "@/assets/about/mobile/image-team-members.jpg";
+import TeamMembersDesktopImage from "@/assets/about/desktop/image-team-members.jpg";
+
+export default function about() {
+  return (
+    <div className="about-main">
+      <NavBar />
+      <main>
+        <section className="flex flex-col gap-14 text-center mt-16 mx-8 mb-20 md:text-left lg:mt-10 lg:mx-60 xl:gap-28">
+          <h1 className="text-4xl text-center tracking-tight text-blue font-bold leading-9 md:text-5xl md:mx-28 lg:text-750 lg:text-left lg:mx-0 lg:pr-[23rem]">
+            We empower innovators by delivering access to the financial system
+          </h1>
+          <div className="flex flex-col gap-6 md:flex-row ">
+            {" "}
+            <h2 className="text-blue text-2xl font-bold md:text-3xl md:w-full lg:w-1/3">
+              Our Vision
+            </h2>
+            <p className="lg:mr-24">
+              Our main goal is to build beautiful consumer experiences along
+              with developer-friendly infrastructure. The result is an
+              intelligent tool that gives everyone the ability to create amazing
+              products that solve big problems. We are deeply focused on
+              democratizing financial services through technology.{" "}
+            </p>
+          </div>
+          <div className="flex flex-col gap-6 md:flex-row">
+            <h2 className="text-blue text-2xl font-bold md:text-3xl md:w-10/12 lg:w-1/4">
+              Our Business
+            </h2>
+            <p>
+              At the core of our platform is the technical infrastructure APIs
+              that connect consumers. Our innovative product provides key
+              insights for businesses and individuals, as well as robust
+              reporting for traditional financial institutions and developers.
+            </p>
+          </div>
+        </section>
+        <section>
+          <Image
+            alt="team members"
+            src={TeamMembersImage}
+            className="w-full md:hidden"
+          />
+          <Image
+            alt="team members"
+            src={TeamMembersDesktopImage}
+            className="hidden md:block md:w-full "
+          />
+          <ul className="flex flex-col justify-center text-center gap-4 border-y-2 border-gray p-6 my-10 mx-10 md:flex-row md:text-left md:border-y-0 md:gap-4 md:mx-6 md:p-0 lg:mx-36">
+            <li className="md:border-y-2 md:pt-4 md:pr-10 md:w-full md:self-end">
+              <h3 className="text-blue font-sans opacity-70">Team Members</h3>
+              <p className="text-dark-pink font-serif text-750 font-bold opacity-100">
+                300+
+              </p>
+            </li>
+            <li className="md:border-y-2 md:pt-4 md:pr-10 md:w-full md:self-end">
+              <h3 className="text-blue font-sans opacity-70">
+                Offices in the US
+              </h3>
+              <p className="text-dark-pink font-serif text-750 font-bold opacity-100">
+                3
+              </p>
+            </li>
+            <li className="md:border-y-2 md:pt-4 md:pr-10 md:w-full md:self-end">
+              <h3 className="text-blue font-sans opacity-70">
+                Transactions analyzed
+              </h3>
+              <p className="text-dark-pink font-serif text-750 font-bold opacity-100">
+                10M+
+              </p>
+            </li>
+          </ul>
+        </section>
+        <section className="flex flex-col text-center justify-center gap-16 mt-16 mx-8 mb-20 md:text-left md:gap-14 lg:mx-56 lg:mt-20">
+          <div className="flex flex-col gap-6 items-center md:flex-row md:gap-6 xl:gap-14 ">
+            <h2 className="text-blue text-2xl font-bold items-center md:text-3xl">
+              The Culture
+            </h2>
+            <p>
+              We strongly believe there's always an opportunity to learn from
+              each other outside of day-to-day work, whether it's company-wide
+              offsites, internal hackathons, or co-worker meetups. We always
+              value cross-team collaboration and diversity of thought, no matter
+              the job title.
+            </p>
+          </div>
+          <div className="flex flex-col gap-6 items-center md:flex-row md:gap-6 lg:gap-10">
+            <h2 className="text-blue text-2xl font-bold md:text-3xl">
+              The People
+            </h2>
+            <p>
+              We're all passionate about building a more efficient and inclusive
+              financial infrastructure together. At PayAPI, we have diverse
+              backgrounds and skills.
+            </p>
+          </div>
+        </section>
+        <CallToAction />
+      </main>
+      <Footer />
+    </div>
+  );
+}
---
+++ src/pages/pricing.tsx
@@ -0,0 +1,154 @@
+import NavBar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
+import { SecondaryButton } from "@/components/UI/Buttons";
+import CallToAction from "@/components/layout/CallToAction";
+import TickIcon from "@/components/UI/TickIcon";
+
+export default function pricing() {
+  return (
+    <div className="pricing-main">
+      <NavBar />
+      <main className="text-blue">
+        <h1 className="text-3xl text-center font-bold md:text-5xl md:pt-6 lg:text-left lg:mx-44 lg:text-750">
+          Pricing
+        </h1>
+        <div className="mb-24 md:flex md:flex-row md:justify-evenly md:gap-4 md:m-20 lg:justify-normal lg:mx-44 xl:gap-28">
+          {" "}
+          <div className="flex flex-col items-center gap-4 lg:items-baseline">
+            <h2 className="text-2xl text-dark-pink p-14 font-bold md:p-0 lg:text-550">
+              Free Plan
+            </h2>
+            <p className="hidden md:block md:text-center lg:text-left">
+              Build and test using our core set of products with up to 100 API
+              requests
+            </p>
+            <h3 className="text-6xl font-bold md:text-5xl md:py-2">$0.00</h3>
+            <ul className="flex flex-col gap-4 border-y-2 border-blue/25 p-6 px-24 md:px-12 lg:pl-0 lg:pr-56">
+              <div className="flex  gap-4">
+                <TickIcon hidden={undefined} />
+                <li>Transactions</li>
+              </div>
+              <div className="flex  gap-4">
+                <TickIcon hidden={undefined} />
+                <li>Auth</li>
+              </div>
+              <div className="flex  gap-4">
+                <TickIcon hidden={undefined} />
+                <li>Identity</li>
+              </div>
+              <div className="flex  gap-4">
+                <TickIcon hidden />
+                <li className="opacity-50">Investments</li>
+              </div>
+              <div className="flex  gap-4">
+                <TickIcon hidden />
+                <li className="opacity-50">Assets</li>
+              </div>
+              <div className="flex  gap-4">
+                <TickIcon hidden />
+                <li className="opacity-50">Liabilities</li>
+              </div>
+              <div className="flex  gap-4">
+                <TickIcon hidden />
+                <li className="opacity-50">Income</li>
+              </div>
+            </ul>
+            <SecondaryButton className="border-blue text-blue lg:mt-3">
+              Request Access
+            </SecondaryButton>
+          </div>
+          <div className="flex flex-col items-center gap-4 lg:items-baseline">
+            <h2 className="text-2xl text-dark-pink p-14 font-bold  md:p-0 lg:text-550">
+              Basic Plan
+            </h2>
+            <p className="hidden md:block md:text-center lg:text-left">
+              Launch your project with unlimited requests and no contractual
+              minimums
+            </p>
+            <h3 className="text-6xl font-bold md:text-5xl md:py-2">$249.00</h3>
+            <ul className="flex flex-col gap-4 border-y-2 border-blue/25 p-6 px-24 md:px-12 lg:pl-0 lg:pr-56">
+              <div className="flex  gap-4">
+                <TickIcon hidden={undefined} />
+                <li>Transactions</li>
+              </div>
+              <div className="flex  gap-4">
+                <TickIcon hidden={undefined} />
+                <li>Auth</li>
+              </div>
+              <div className="flex  gap-4">
+                <TickIcon hidden={undefined} />
+                <li>Identity</li>
+              </div>
+              <div className="flex  gap-4">
+                <TickIcon hidden={undefined} />
+                <li>Investments</li>
+              </div>
+              <div className="flex  gap-4">
+                <TickIcon hidden={undefined} />
+                <li>Assets</li>
+              </div>
+              <div className="flex  gap-4">
+                <TickIcon hidden />
+                <li className="opacity-50">Liabilities</li>
+              </div>
+              <div className="flex  gap-4">
+                <TickIcon hidden />
+                <li className="opacity-50">Income</li>
+              </div>
+            </ul>
+            <SecondaryButton className="border-blue text-blue lg:mt-3">
+              Request Access
+            </SecondaryButton>
+          </div>
+          <div className="flex flex-col items-center gap-4 lg:items-baseline">
+            <h2 className="text-2xl text-dark-pink p-14 font-bold md:p-0 lg:text-550">
+              Premium Plan
+            </h2>
+            <p className="hidden md:block md:text-center lg:text-left">
+              Get tailored solutions, volume pricing, and dedicated support for
+              your team
+            </p>
+            <h3 className="text-6xl text-blue font-bold md:text-5xl md:py-2">
+              $499.00
+            </h3>
+            <ul className="flex flex-col gap-4 border-y-2 border-blue/25 p-6 px-24 md:px-12 lg:pl-0 lg:pr-56">
+              <div className="flex  gap-4">
+                <TickIcon hidden={undefined} />
+                <li>Transactions</li>
+              </div>
+              <div className="flex  gap-4">
+                <TickIcon hidden={undefined} />
+                <li>Auth</li>
+              </div>
+              <div className="flex  gap-4">
+                <TickIcon hidden={undefined} />
+                <li>Identity</li>
+              </div>
+              <div className="flex  gap-4">
+                <TickIcon hidden={undefined} />
+                <li>Investments</li>
+              </div>
+              <div className="flex  gap-4">
+                <TickIcon hidden={undefined} />
+                <li>Assets</li>
+              </div>
+              <div className="flex  gap-4">
+                <TickIcon hidden={undefined} />
+                <li>Liabilities</li>
+              </div>
+              <div className="flex  gap-4">
+                <TickIcon hidden={undefined} />
+                <li>Income</li>
+              </div>
+            </ul>
+            <SecondaryButton className="border-blue text-blue lg:mt-3">
+              Request Access
+            </SecondaryButton>
+          </div>
+        </div>
+        <CallToAction />
+      </main>
+      <Footer />
+    </div>
+  );
+}
