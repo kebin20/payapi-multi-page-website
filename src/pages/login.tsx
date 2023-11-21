41d212cd4edacf2354e03d4a97a379757dfb0b91
kebin20 <ktanzyl@hotmail.com>
2023-11-21 13:39:09 +0900

    feat: setup authentication with kinde

---
+++ src/pages/login.tsx
@@ -0,0 +1,12 @@
+import Footer from "@/components/layout/Footer";
+import NavBar from "@/components/layout/Navbar";
+
+export default function Login() {
+  return (
+    <>
+      <NavBar />
+      <h1>Admin Login Page</h1>
+      <Footer />
+    </>
+  );
+}
