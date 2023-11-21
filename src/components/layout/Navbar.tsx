581c88737a8928696e122dc303225609e9ce9916
kebin20 <ktanzyl@hotmail.com>
2023-11-21 13:39:19 +0900

    chore: renamed link names to lowercase

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -13,7 +13,7 @@ export default function NavBar() {
     setToggle((prevToggle) => !prevToggle);
   }
 
-  const navItems = ["Pricing", "About", "Contact"];
+  const navItems = ["pricing", "about", "contact"];
 
   return (
     <header
@@ -37,7 +37,7 @@ export default function NavBar() {
               href={`/${item}`}
               className="md:text-blue"
             >
-              {item}
+              {item.charAt(0).toUpperCase() + item.slice(1)}
             </NavigationItem>
           ))}
         </ul>
